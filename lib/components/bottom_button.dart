08b3a0abc13bc8c4c7677f744fef8144cfb4e5ff
natanim-samuel <ntnmsamuel@gmail.com>
2026-09-10 19:38:19 +0300

    bottom_botton

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({
+    super.key,
+    required this.buttonTitle,
+    required this.onPressed,
+  });
+
+  final String buttonTitle;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
