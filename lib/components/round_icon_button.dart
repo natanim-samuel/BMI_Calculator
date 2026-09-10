8a7eaafb68059265f3921ee4f6084c0e93a5e44d
natanim-samuel <ntnmsamuel@gmail.com>
2026-09-10 19:43:55 +0300

    round icon button

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+  });
+
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 0.0,
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      child: FaIcon(icon, color: Colors.white),
+      onPressed: onPressed,
+    );
+  }
+}
